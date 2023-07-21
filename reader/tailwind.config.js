30c2391de2a79f68c795782a726fd8e89b051201
khang-07 <khangnguyen_2007@icloud.com>
2023-07-21 16:53:09 -0500

    jul21 with tailwind

---
+++ reader/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  mode: "jit",
+  content: [
+    "./src/App.tsx",
+    "./src/main.tsx",
+    "./index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
